6df9abe1b0a7cdb8787e7d264e828313915da033
Audra Huffmeyer <ahuffmeyer@ucla.edu>
2015-09-24 16:37:59 -0400

    SNP_work

--- My_first_script.R
+++ My_first_script.R
@@ -130,3 +130,57 @@ plot(1:11, NN, xlab="time", ylab="N", type="b", col="blue")
 
 for(i in 1:10)
 {cat("*&")}
+
+dogs <- 10;
+for (i in 1:5){
+  dogs <- dogs + 1; 
+}
+###
+meatloaf <- 0; 
+for (i in 5:9){
+  meatloaf <- meatloaf - i + 1;
+  cat(meatloaf) 
+}
+###
+bubbles <- 12;
+for (i in -1:-4){
+  bubbles <- i;
+}
+
+bankAccounts <- c(10, 9.2, 5.6, 3.7, 8.8, 0.5);
+
+#Now look at the error message the following lines of code produce. Can you think of a way to modify this loop so that the loop will compund the interest?
+
+interestRate <- 0.0125;
+for (c in 1:length(bankAccounts)) {
+  compounded[c] <- interestRate*bankAccounts[c] + bankAccounts[c]; }
+c
+bankAccounts
+
+bankAccounts <- c(10, 9.2, 5.6); #define bank accounts here
+interestRate <- 0.0525;   
+house <- c(4.8, 3.8, 5.7); #deduct
+food<- c(3.5, 4.3, 5.0);    #deduct
+fun <- c(7.8, 2.1, 10.5);  #deduct
+#and incomes (through TAships) of 
+income <- c(21, 21, 21); #add this
+
+for (j in 1:5) {
+  for (i in 1:length(bankAccounts)) {
+    #step 1 modify bankAccounts so that amounts reflect income and expenses
+    #step 2 get calculate interest and add to accounts from step 1
+    #you can actually use the line you have already written if you
+    #modify amounts in bankAccounts directly in step 1
+  }
+bankAccounts <- c(10, 9.2, 5.6); #define bank accounts here
+interestRate <- 0.0525;   
+house <- c(4.8, 3.8, 5.7); #deduct
+food<- c(3.5, 4.3, 5.0);    #deduct
+fun <- c(7.8, 2.1, 10.5);  #deduct
+bankAccounts-house
+bankAccounts-house
+bankAcounts/2
+
+bankAccounts <- c(10, 9.2, 5.6)
+house <- c(4.8, 3.8, 5.7)
+bankAccounts-house
